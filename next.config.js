8b4bb743d89072551c18917b3a437b1c43d9497e
Septem Li <1216949+septemhill@users.noreply.github.com>
2025-09-12 16:16:14 +0800

    Fix: Configure Next.js for static export and unoptimized images
    This commit configures the Next.js application for static export by setting `output: 'export'`. Additionally, it enables `unoptimized: true` for images to ensure compatibility with the static export configuration. This change allows the application to be deployed on static hosting platforms.

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const { i18n } = require('./next-i18next.config.js');
 
 const nextConfig = {
   // 如果您有其他的 Next.js 設定，請保留它們
-  i18n,
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
